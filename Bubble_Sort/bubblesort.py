c8ca8cd6f97115b5d520d8d4af052ff4e9814f72
hridya <hridyakrishnar127@gmail.com>
2020-10-05 16:08:02 +0530

    bubblesort.py added

---
+++ Bubble_Sort/bubblesort.py
@@ -0,0 +1,14 @@
+a = []					
+number = int(input("Enter the total number of elements: "))	#Enter the number of inputs.
+for i in range(number):
+    value = int(input(%i))		#Input the elements 
+    a.append(value)			#appending elements to the array
+
+for i in range(number - 1):		#traverses throughout the array
+    for j in range(number - i - 1):	#this loop swaps if the element is greater than the next element 
+        if(a[j] > a[j + 1]):
+             temp = a[j]
+             a[j] = a[j + 1]
+             a[j + 1] = temp
+
+print("The Sorted List in Ascending Order : ", a) #Prints the sorted list
