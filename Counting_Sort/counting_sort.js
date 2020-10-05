26b8a2069c4c97b0625a3abc5511c8879a8c2412
SHWETABH SAXENA <46630470+SHARC-99@users.noreply.github.com>
2020-10-05 23:02:29 +0530

    Counting_Sort.js
    Bug resolved:- updated code is running perfectly

--- Counting_Sort/counting_sort.js
+++ Counting_Sort/counting_sort.js
@@ -1,20 +1,22 @@
-let countingSort = (arr, min, max) => {
-    let i = min,
-        j = 0,
-        len = arr.length,
-        count = [];
-    for (i; i <= max; i++) {
-        count[i] = 0;
+function countingSort(arr){
+  var helper = []; 
+  for(var i = 0; i<arr.length; i++){
+    if(!helper[arr[i]]){
+        helper[arr[i]] = 1;
+    }else{
+        helper[arr[i]] += 1;
     }
-    for (i = 0; i < len; i++) {
-        count[arr[i]] += 1;
+  }
+
+  var newArr = []; 
+  for(i in helper){
+    while(helper[i]>0){
+        newArr.push(parseInt(i));
+        helper[i]--;
     }
-    for (i = min; i <= max; i++) {
-        while (count[i] > 0) {
-            arr[j] = i;
-            j++;
-            count[i]--;
-        }
-    }
-    return arr;
-};
+  }
+  return newArr; 
+}
+
+var arr = [5,4,3,2,1,0];
+console.log(countingSort(arr)); // [0, 1, 2, 3, 4, 5]
