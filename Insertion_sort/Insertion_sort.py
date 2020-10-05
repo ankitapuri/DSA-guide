0678b4570a9379f902ea56d0117f99c2a5057a6a
tahmid02016 <55309419+tahmid02016@users.noreply.github.com>
2020-10-05 17:33:30 +0600

    Code Debugged

--- Insertion_sort/Insertion_sort.py
+++ Insertion_sort/Insertion_sort.py
@@ -2,12 +2,10 @@ def insertion_sort (L):
     n = len (L)
     for index in range (1, n):
         item = L [index]
-        j = index - 1
-        while j >= 0 and L[j] > item:
-            L [j+1] = L [j]
-            j = j-1
+        j = index - 1                                   while j >= 0 and L[j] > item:
+            L [j+1] = L [j]                                 j = j-1
         L [j+1] = item
-if __name_ == "__main__":
-    L = input().splice()
+if __name__ == "__main__":
+    L = input().split()
     insertion_sort (L)
     print (L)
