1b5c2a9ba49c2879d1b7ccb13fedd61018c1e438
Archana Dhaker <archana2561996@gmail.com>
2020-10-05 21:09:59 +0530

    Quick Sort in Python

--- quick_sort/QuickSort.py
+++ quick_sort/QuickSort.py
@@ -1,27 +1,26 @@
-def QuickSort(arr):
-
-    elements = len(arr)
-    
-    #Base case
-    if elements < 2:
-        return arr
-    
-    current_position = 0 #Position of the partitioning element
-
-    for i in range(1, elements): #Partitioning loop
-         if arr[i] <= arr[0]:
-              current_position += 1
-              temp = arr[i]
-              arr[i] = arr[current_position]
-              arr[current_position] = temp
-
-    temp = arr[0]
-    arr[0] = arr[current_position] 
-    arr[current_position] = temp #Brings pivot to it's appropriate position
-    
-    left = QuickSort(arr[0:current_position]) #Sorts the elements to the left of pivot
-    right = QuickSort(arr[current_position+1:elements]) #sorts the elements to the right of pivot
-
-    arr = left + [arr[current_position]] + right #Merging everything together
-    
-    return arr
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quickSort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quickSort(arr, low, pi-1)
+      quickSort(arr, pi+1, high)
+# main
+arr = [2,5,3,8,6,5,4,7]
+n = len(arr)
+quickSort(arr,0,n-1)
+print ("Sorted array is:")
+for i in range(n):
+   print (arr[i],end=" ")
