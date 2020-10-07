ff76aab625fe6a721232f0df697da78045268000
Samriddhi Arya <samriddhiarya@gmail.com>
2020-10-07 19:47:29 +0530

    Added comments

--- Recursion/Java/sum_k.java
+++ Recursion/Java/sum_k.java
@@ -1,3 +1,14 @@
+/***
+ * sum_k program implements the code to print all the subsets of the given array
+ * whose sum equals k.
+ * 
+ * eg.	arr = {1,2,3,4}
+ * 		k = 4;
+ * 
+ * 		Subsets whose sum equals k are {4}, {1,3}
+ */
+
+
 import java.util.Scanner;
 
 public class sum_k {
@@ -6,33 +17,56 @@ public class sum_k {
 		
 		Scanner s = new Scanner(System.in);
 		
-		//Size of the array
+		//Enter size of the array
 		int n = s.nextInt();
 		
-		//value of k
+		//Enter value of k
 		int k = s.nextInt();
 		
+		//Enter array values
 		int[] arr = new int[n];
 		for(int i = 0;i < n;i++) {
 			arr[i] = s.nextInt();
 		}
+		
+		//Function call 
 		int[][] ans = subsetsSumK(arr, k);
+		
+		//Print the values of subsets
 		for(int i = 0;i <ans.length;i++) {
 			for(int j = 0;j <ans[i].length;j++)
 				System.out.print(ans[i][j] + " ");
 			System.out.println();
 		}
+		
 		s.close();
 	}
+	
+	
+	/***
+	 * 
+	 * @param input: input array
+	 * @param k: target sum value
+	 * @return: 2D array containing all the subsets of input array whose sum equals k
+	 */
+	public static int[][] subsetsSumK(int input[], int k) {
 
-		public static int[][] subsetsSumK(int input[], int k) {
-
+		  //get all the subsets of given input array
 	      int[][] arr = sub(input,0,k);
-	      
+	     
+	      //return subsets whose sum of elements equals k
 	      return subSumK(arr,k,0);
-		}
+	}
 	  
-	  public static int[][] sub(int[] input,int index,int k){
+
+	/***
+	 *  
+	 * @param input: input array
+	 * @param index: index value to be considered in input array
+	 * @param k: target sum value
+	 * @return: 2D array of all the subsets of given input array
+	 */
+	public static int[][] sub(int[] input,int index,int k){
 	      
 	      if(index == input.length){
 	        int[][] arr = new int[1][0];
@@ -51,7 +85,7 @@ public class sum_k {
 	          sum = sum + result[i][j];
 	        }
 	        if(sum == k) {
-	        	int[][] finalAns = get(result[i]);
+	        	int[][] finalAns = null;
 	        }
 	      }
 	      
@@ -63,14 +97,17 @@ public class sum_k {
 	        }
 	      }
 	      return result;
-	    }
+	 }
 	  
-	    public static int[][] get(int[] is) {
-	    	
-		return null;
-	}
 
-		public static int[][] subSumK(int[][] arr,int k,int index){
+	/***
+	 * 
+	 * @param arr: 2D array of all the subsets of given input array
+	 * @param k: target sum value
+	 * @param index: index value 
+	 * @return: 2D array containing all the subsets of input array whose sum equals k
+	 */
+	public static int[][] subSumK(int[][] arr,int k,int index){
 	      
 	      
 	      if(index == arr.length){
@@ -98,6 +135,6 @@ public class sum_k {
 	        return result;
 	      }
 	      return smallAns;
-	    }
+	 }
 	
 }
