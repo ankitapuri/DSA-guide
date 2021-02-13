5a587bc8367a3bcf46311fc4100d8822bcb6eb70
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 21:59:36 +0530

    #164 added comments to fibonacci.c

--- Recursion/C/fibonacci.c
+++ Recursion/C/fibonacci.c
@@ -1,23 +1,25 @@
+//TODO: Print fibonacci series
+
 #include<stdio.h>
 #include<stdlib.h>
 
  int fibonacci(int n) 
  { 
      if (n <= 1) 
-         return n; 
+         return n;                                  //TODO: Base statement
      else
-         return fibonacci(n-1) + fibonacci(n-2); 
+         return fibonacci(n-1) + fibonacci(n-2);    //TODO: Recursive statement
  } 
 
  int main () 
  { 
      int n;
      printf("Enter n: ");
-     scanf("%d",&n);
+     scanf("%d",&n);                                //TODO: Getting user input
      int i=0;
      while(i<n)
      {
-     	printf("%d\n",fibonacci(i));
+     	printf("%d\n",fibonacci(i));                  //TODO: Printing result and calling recursive function
          i++;
      }
      return 0; 
