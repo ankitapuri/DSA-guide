cd03d2412299c03e3d762d47e7a0186b61db2234
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 22:02:33 +0530

    #164 added comments to factorial.js

--- Recursion/JS/factorial.js
+++ Recursion/JS/factorial.js
@@ -1,24 +1,25 @@
-// program to find the factorial of a number
+//TODO: Find the factorial of a given number
+
 function factorial(x) {
 
     // if number is 0
-    if (x == 0) {
+    if (x == 0) {                             //TODO: Base statement
         return 1;
     }
 
     // if number is positive
     else {
-        return x * factorial(x - 1);
+        return x * factorial(x - 1);          //TODO: Recursive statement
     }
 }
 
-// take input from the user
+                                              //TODO: take input from the user
 let num = prompt('Enter a positive number: ');
 
-// calling factorial() if num is positive
+                                              //TODO:calling factorial() if num is positive
 if (num >= 0) {
     let result = factorial(num);
-    console.log(`The factorial of ${num} is ${result}`);
+    console.log(`The factorial of ${num} is ${result}`);    //TODO: Print result
 }
 else {
     console.log('Enter a positive number.');
