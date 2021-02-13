a0f2a5a5fa1a437e248b16c283d0e704e8df17d3
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 21:49:27 +0530

    #164 added comments to stringpalindrome.cpp

--- Recursion/C++/stringPalindrome.cpp
+++ Recursion/C++/stringPalindrome.cpp
@@ -1,3 +1,5 @@
+//TODO: Check if the given string is palindrome
+
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
@@ -10,10 +12,10 @@ int main()
     string s;
 
     cout << "Enter a String to check if it is Palindrome: ";
-    cin >> s;
+    cin >> s;                                                    //TODO: Taking user input
 
     if(palindrome(s))
-        cout << s << " is a Palindrome string";
+        cout << s << " is a Palindrome string";                  //TODO: Printing result
     else
         cout << s << " is not a Palindrome string";
 
@@ -24,9 +26,9 @@ bool palindrome(string s)
 {
     int sz = s.size();
 
-    if(sz <=1) return true;
+    if(sz <=1) return true;                                      //TODO: Base statement
 
-    if((s[0] == s[sz-1]) and palindrome(s.substr(1,sz-2)))
+    if((s[0] == s[sz-1]) and palindrome(s.substr(1,sz-2)))       //TODO: Recursive statement
     	return true;
 
     return false;
