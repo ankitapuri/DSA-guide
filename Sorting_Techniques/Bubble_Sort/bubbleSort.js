7e1cb556ee30f5b851557ebb80c1abb747bd8425
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 21:02:44 +0530

    #164 added comments to bubblesort.js

--- Sorting_Techniques/Bubble_Sort/bubbleSort.js
+++ Sorting_Techniques/Bubble_Sort/bubbleSort.js
@@ -1,4 +1,4 @@
-//ES6 syntax, doube arrow functions
+                                     //TODO: ES6 syntax - doube arrow functions
 let bubbleSort = (arr) => {
     let len = arr.length;
     for (let i = 0; i < len; i++) {
@@ -13,6 +13,6 @@ let bubbleSort = (arr) => {
     return arr;
 };
 
-//driver to test function
+                                      //TODO: driver to test function
 bubbleSort([2,3,34,4,5,3,1,213,13,2])
-//output: [1, 2, 2, 3, 3, 4, 5, 13, 34, 213]
+                                      //TODO:Print output: [1, 2, 2, 3, 3, 4, 5, 13, 34, 213]
