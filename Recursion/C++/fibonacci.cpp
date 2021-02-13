9d29d8ac026710623fcf5ccfe59240734f74bef5
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 21:43:36 +0530

    #164 added comments to fibonacci.cpp

--- Recursion/C++/fibonacci.cpp
+++ Recursion/C++/fibonacci.cpp
@@ -1,22 +1,24 @@
+//TODO: Print fibonacci series
+
 #include<bits/stdc++.h> 
 using namespace std;   
 
 int fibonacci(int n) 
 { 
     if (n <= 1) 
-        return n; 
+        return n;                                    //TODO: Base statement
     else
-        return fibonacci(n-1) + fibonacci(n-2); 
+        return fibonacci(n-1) + fibonacci(n-2);      //TODO: Recursive statement
 } 
 
 int main () 
 { 
-    int n;
+    int n;                                           //TODO: Getting user input
     cin>>n;
     int i=0;
     while(i<n)
     {
-        cout << fibonacci(i)<<"\n"; 
+        cout << fibonacci(i)<<"\n";                  //TODO: Printing result and calling the recursive function
         i++;
     }
     return 0; 
