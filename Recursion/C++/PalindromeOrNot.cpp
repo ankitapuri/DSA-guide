6e04f350b2e6ed869809c02ed89d14892e83bafe
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 21:35:22 +0530

    #164 added/updated comments of palindromeornot.cpp

--- Recursion/C++/PalindromeOrNot.cpp
+++ Recursion/C++/PalindromeOrNot.cpp
@@ -1,24 +1,23 @@
-// A recursive C++ program to check whether a given number is 
-// palindrome or not 
+//TODO: A recursive C++ program to check whether a given number is palindrome or not 
+
 #include<bits/stdc++.h>
 #include <stdio.h> 
 using namespace std;
-// A function that reurns true only if num contains one digit 
+//TODO: Write A function that reurns true only if num contains one digit 
 int oneDigit(int num) 
 { 
-	// comparison operation is faster than division operation. 
-	// So using following instead of "return num / 10 == 0;" 
+	//TODO: comparison operation is faster than division operation. 
+	//TODO: So using following instead of "return num / 10 == 0;" 
 	return (num >= 0 && num < 10); 
 } 
 
-// A recursive function to find out whether num is palindrome 
-// or not. Initially, dupNum contains address of a copy of num. 
+ 
 bool isPalUtil(int num, int* dupNum) 
 { 
-	// Base case (needed for recursion termination): This statement 
-	// mainly compares the first digit with the last digit 
+	
+	
 	if (oneDigit(num)) 
-		return (num == (*dupNum) % 10); 
+		return (num == (*dupNum) % 10); // Base case (needed for recursion termination): This statement mainly compares the first digit with the last digit 
 
 	// This is the key line in this method. Note that all recursive 
 	// calls have a separate copy of num, but they all share same copy 
@@ -26,8 +25,8 @@ bool isPalUtil(int num, int* dupNum)
 	if (!isPalUtil(num/10, dupNum)) 
 		return false; 
 
-	// The following statements are executed when we move up the 
-	// recursion call tree 
+	// The following statements are executed when we move up the recursion call tree
+	 
 	*dupNum /= 10; 
 
 	// At this point, if num%10 contains i'th digit from beiginning, 
@@ -35,16 +34,16 @@ bool isPalUtil(int num, int* dupNum)
 	return (num % 10 == (*dupNum) % 10); 
 } 
 
-// The main function that uses recursive function isPalUtil() to 
-// find out whether num is palindrome or not 
+        //TODO: The main function that uses recursive function isPalUtil() to 
+        //TODO: find out whether num is palindrome or not 
 int isPal(int num) 
 { 
-	// If num is negative, make it positive 
+	//TODO: If num is negative, make it positive 
 	if (num < 0) 
 	num = -num; 
 
-	// Create a separate copy of num, so that modifications made 
-	// to address dupNum don't change the input number. 
+	//TODO: Create a separate copy of num, so that modifications made to address dupNum don't change the input number. 
+	
 	int *dupNum = new int(num); // *dupNum = num 
 
 	return isPalUtil(num, dupNum); 
