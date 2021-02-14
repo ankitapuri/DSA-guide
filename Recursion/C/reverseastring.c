90353b526efd19589fc04c4c70f99ac2f41dd430
abhinav karforma <abhinavkarforma2@gmail.com>
2021-02-14 23:50:18 +0530

    Reverse a string in C

--- Recursion/C/reverseastring.c
+++ Recursion/C/reverseastring.c
@@ -2,30 +2,29 @@
 #include<stdio.h>
 #include<string.h>
 int a = 0;
-//reverse_the_string function is used to reverse a string
-void reverse_the_string(char str[60],int length)
+                                                           //reverse_the_string function is used to reverse a string
+void reverse_the_string(char str[60],int length)                             ////TODO: reverse a string using recursion
 {
     char temp;
     temp = str[a];
-    str[a] = str[length-1];
+    str[a] = str[length-1];                                       //TODO: Swapping string
     str[length-1] = temp;
     if(a < length/2)
     {
         ++a;
         reverse_the_string(str,--length);
-    }
-        //printing the reversed string
+    }                                                               
     else 
-    printf("Reversed string = %s",str);
+    printf("Reversed string = %s",str);                                       //printing the reversed string
     
 }
-int main()
+int main()                                                                           //TODO: Driver code 
 {
     char string[60];
     int length;
     printf("Enter a string: ");
     scanf("%s",string);
     length = strlen(string);
-    reverse_the_string(string,length);
+    reverse_the_string(string,length);                                      ////TODO: Calling reverse Function
     return 0;
 }
