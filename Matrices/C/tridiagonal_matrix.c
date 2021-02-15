db6777cf9a9bc47b23a14744ab5399dc1fd5cb99
Deepak Verma <0822deepakverma@gmail.com>
2021-02-15 19:21:03 +0530

    updated comments in the code

--- Matrices/C/tridiagonal_matrix.c
+++ Matrices/C/tridiagonal_matrix.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
 
 int main()
-{ //Declaring variables
+{ 
+  //Declaring variables
   int i,j,k=0 ,order;
+  
+  //taking matrix order as input
   printf("Enter the order of the matrix: ");
-  scanf("%d",&order);   //taking order input
+  scanf("%d",&order);   
+  
   int num;
+  
+  //calculating number of non-zero elements 
   num=3*order-2;
+  
+  //declaration of matrix
   int matrix[num];
+  
+  //taking input of all non-zero elements
   printf("Enter %d  non-zero elements: ",num);
   for(i=0;i<num;i++)
-  scanf("%d",&matrix[i]);    //Non zero values of the matrix
-
-
+  scanf("%d",&matrix[i]);   
+  
+  //printing the tridiagonal matrix 
   printf("Tridiagonal matrix formed is: \n");
   for(i=1;i<=order;i++)
   {
