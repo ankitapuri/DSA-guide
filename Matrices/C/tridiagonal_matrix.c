efefef845f01a90e2bf5b60d3426fcdd6b3e3da6
Deepak Verma <0822deepakverma@gmail.com>
2021-02-15 20:27:23 +0530

    updated the code

--- Matrices/C/tridiagonal_matrix.c
+++ Matrices/C/tridiagonal_matrix.c
@@ -1,40 +1,55 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 int main()
-{ 
+{
   //Declaring variables
-  int i,j,k=0 ,order;
-  
-  //taking matrix order as input
-  printf("Enter the order of the matrix: ");
-  scanf("%d",&order);   
-  
+  int i,k=0,j,size;
+  int *matrix;
+
+  //taking input of the order of matrix
+  printf("\nEnter order of matrix: " );
+  scanf("%d", &size);
+
+  //calculation no of non zero elements to be entered
   int num;
-  
-  //calculating number of non-zero elements 
-  num=3*order-2;
-  
-  //declaration of matrix
-  int matrix[num];
-  
-  //taking input of all non-zero elements
-  printf("Enter %d  non-zero elements: ",num);
+  num=3*size-2;
+
+  //creating matrix
+  matrix = (int *)malloc(size * sizeof(int));
+
+  //enter non zero element
+  printf("\nEnter %d  non-zero elements: ",num);
+    printf("\n" );
   for(i=0;i<num;i++)
-  scanf("%d",&matrix[i]);   
-  
-  //printing the tridiagonal matrix 
-  printf("Tridiagonal matrix formed is: \n");
-  for(i=1;i<=order;i++)
+   {
+     printf("\nEnter element %d : ", i+1 );
+     scanf("%d", &matrix[i]);
+   }
+
+  //printing matrix
+  printf("\nTridiagonal matrix formed is: \n");
+  for(i=1;i<=size;i++)
   {
-      for(j=1;j<=order;j++)
-     { if((i-j)==0)
-       printf("%d ",matrix[k++]);
+      for(j=1;j<=size;j++)
+     {
+       if((i-j)==0)
+       {
+         printf("%d ",matrix[k++]);
+       }
        else if((i-j)==-1)
-       printf("%d ",matrix[k++]);
+       {
+         printf("%d ",matrix[k++]);
+       }
        else if((i-j)==1)
-       printf("%d ",matrix[k++]);
+       {
+         printf("%d ",matrix[k++]);
+       }
        else
-       printf("%d ",0);
+       {
+         printf("%d ",0);
+       }
      }
      printf("\n");
   }
