023ff416a4c2c10c9c5637345cb15167ca247179
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-17 11:20:31 +0530

    Create Bead Sort.py

---
+++ Sorting_Techniques/Bead_Sort/Bead Sort.py
@@ -0,0 +1,22 @@
+def beadsort(input_list):
+    return_list = []
+    transposed_list = [0] * max(input_list)                            #TODO: column of input beads and laying it out flat
+
+    for num in input_list:
+        transposed_list[:num] = [n + 1 for n in transposed_list[:num]] #TODO: Drop the beads. To de-transpose
+
+    for _ in input_list:
+         is how we tell how many beads are in the
+        return_list.append(sum(n > 0 for n in transposed_list))        #TODO: Counting values > 0; True == 1 and False == 0.
+        transposed_list = [n - 1 for n in transposed_list]             #TODO: Remove the 'bottommost row' by subtracting 1 from each element.
+
+    return return_list                                                 #TODO: The resulting list is sorted in descending order
+
+n = int(input("Enter length of the array: "))                          #TODO: Get length of array
+arr = []
+print("Enter each element of array and press Enter - \n")              #TODO: Get the elements of array
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+farr = beadsort(arr)                                                   #TODO: Calling beadsort function
+print(farr)                                                            #TODO: Printing sorted array
