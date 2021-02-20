8169492c8b8ad12940fea7980f49359b12479623
Amit kumar mishra <akm43300@gmail.com>
2021-02-20 12:13:54 +0530

    Tridiagonal

--- Matrices/C++/Tridiagonal_matrices.cpp
+++ Matrices/C++/Tridiagonal_matrices.cpp
@@ -3,40 +3,56 @@
 #include <iostream>
 using namespace std;
 
-void main()
+int main()
 {
-int i,j,k;
-int mat[5][5]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-//entered matrix
-cout<<"Entered matrix is:\n\n";
-for(i=0;i<5;i++)
-    {
-    for(j=0;j<5;j++)
-        {
-        cout<<mat[i][j]<<endl;
-        }
-    printf("\n");
-    }
-//Display the Tridiagnal matrix
-cout<<"\nTridiagnal matrix:\n\n";
-cout<<mat[0][0]<<"\t"<<mat[0][1]<<endl;
-for(i=1;i<5;i++)
-    {
-    for(j=1;j<i;j++)
-        {
-        cout<<"\t";
-        }
-    if(i==4)
-        cout<<mat[4][3]<<"\t"<<mat[4][4]<<endl;
-    else
-        {
-        for(k=i;k<5;k++)
-            {
-            if(i==k)
-            //tridiagonal element found
-            cout<<mat[i][k-1]<<"\t"<<mat[i][k]<<"\t"<<mat[i][k+1]<<endl;
-            }
-        cout<<endl;
-        }
-    }
+  //Declaring variables
+  int i,k=0,j,size;
+  int *mat;
+
+  //taking input of the order of matrix
+  cout<<"\nEnter order of matrix: " <<endl;
+  cin>>size;
+
+  //calculation no of non zero elements to be entered
+  int num;
+  num=3*size-2;
+
+  //creating matrix
+  mat = (int *)malloc(size * sizeof(int));
+
+  //enter non zero element
+  cout<<"Enter "<<num<<"  non-zero elements: "<<endl;
+    
+  for(i=0;i<num;i++)
+   {
+     cout<<"Enter element "<<i+1<<" : "<<endl;
+     cin>>matrix[i];
+   }
+
+  //printing matrix
+  cout<<"Tridiagonal matrix formed is: "<<endl;
+  for(i=1;i<=size;i++)
+  {
+      for(j=1;j<=size;j++)
+     {
+       if((i-j)==0)
+       {
+         cout<<mat[k++]<<" ";
+       }
+       else if((i-j)==-1)
+       {
+         cout<<mat[k++]<<" ";
+       }
+       else if((i-j)==1)
+       {
+         cout<<mat[k++]<<" ";
+       }
+       else
+       {
+         cout<<0<<" ";
+       }
+     }
+     cout<<endl;
+  }
+  return 0;
 }
