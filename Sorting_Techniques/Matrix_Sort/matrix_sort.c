05d81942234fb286f118d8239bad90d6d48c0840
vybhav72954 <vybhavchaturvedi@gmail.com>
2021-03-07 22:50:33 +0530

    Added comments in appropriate places

--- Sorting_Techniques/Matrix_Sort/matrix_sort.c
+++ Sorting_Techniques/Matrix_Sort/matrix_sort.c
@@ -10,6 +10,16 @@ Matrix Sorting in C
 
 int main()
 {
+    /*
+     ROW: Number of Rows
+     COLUMN: Number of Columns
+     i, j = Iterators
+     temp = temporary Variable to swap the values
+     k = iteration for row iterator i
+     w = for storing the previous value of column iterator j
+     z = used as a temporary variable for storing k
+     q = used as temporary variable for storing column iterator j
+     */
     int ROW, COLUMN;
     printf("Row: ");
     scanf("%d", &ROW);
@@ -18,33 +28,44 @@ int main()
     printf("Enter the elements:\n");
     int arr[ROW][COLUMN], minimum, i, j, temp, k, w, z = 0, q = 0 ;
 
-
+    // Input values in a ROW*COLUMN form
     for(i = 0;i < ROW;i++)
         for(j = 0;j < COLUMN;j++)
         {
             scanf("%d",&arr[i][j]);
         }
 
-
+    // Traverse over the Matrix
     for(i = 0;i < ROW;i++)
         for(j = 0;j < COLUMN;j++)
         {
             temp = i;
             q = j;
+
+            // Current minimum element
             minimum = arr[i][j];
+
+            // Check if any smaller element is present in the matrix
             w = j;
+
             for(k = i;k < ROW;k++)
             {
 
-                for(;w < COLUMN;w++)
-                if(arr[k][w] < minimum)
-                {
-                    minimum = arr[k][w];
-                    z = k;
-                    q = w;
+                for(;w < COLUMN;w++) {
+
+                    // Update the minimum element
+                    if(arr[k][w] < minimum)
+                    {
+                        // Update the index of the minimum element
+                        minimum = arr[k][w];
+                        z = k;
+                        q = w;
+                    }
                 }
                 w = 0;
             }
+
+            // Swap the current element and the minimum element
             if(arr[z][q] < arr[i][j])
             {
                 temp = arr[i][j];
@@ -53,6 +74,7 @@ int main()
             }
         }
 
+    // Print the Sorted Matrix
     printf("\nMatrix After Sorting: \n");
     for(i = 0; i < ROW; i++)
     {
