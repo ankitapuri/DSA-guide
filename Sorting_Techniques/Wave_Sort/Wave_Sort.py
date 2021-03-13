2d2ea5447d82f8745eda3827d44ee5d9c2c769bf
96RadhikaJadhav <radhikajadhav014@gmail.com>
2021-03-14 01:36:25 +0530

    Added user input functionality

--- Sorting_Techniques/Wave_Sort/Wave_Sort.py
+++ Sorting_Techniques/Wave_Sort/Wave_Sort.py
@@ -1,16 +1,31 @@
 # Python function to sort the array arr[0..n-1] in wave form, 
 # i.e., arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= arr[5] 
-def sortInWave(arr, n): 
-      
-    #sort the array 
-    arr.sort() 
-     
-    # Swap adjacent elements 
-    for i in range(0,n-1,2): 
-        arr[i], arr[i+1] = arr[i+1], arr[i] 
-  
+arr=[]
+#Enter the number of inputs.
+n = int(input("Enter the total number of elements: "))	
+print("Enter the inputs:")
+for i in range(n):
+
+    #Input the elements 
+    value = int(input())
+
+    #appending elements to the array				
+    arr.append(value)					
+
+for i in range(n):
+    def sortInWave(arr, n): 
+        
+        #sort the array 
+        arr.sort() 
+        
+        # Swap adjacent elements 
+        for i in range(0,n-1,2): 
+            arr[i], arr[i+1] = arr[i+1], arr[i] 
+    
 # Driver program 
-arr = [10, 90, 49, 2, 1, 5, 23] 
+ 
 sortInWave(arr, len(arr)) 
+print("The array after sorting is:")
 for i in range(0,len(arr)): 
-    print(arr[i]) 
+    print(arr[i])
+    
