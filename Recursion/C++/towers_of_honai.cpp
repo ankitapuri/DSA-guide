b26fa7872baf49d48bbe52d3ae6af4b66bd423bd
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2021-03-14 12:42:50 +0530

    Update towers_of_honai.cpp

--- Recursion/C++/towers_of_honai.cpp
+++ Recursion/C++/towers_of_honai.cpp
@@ -1,7 +1,7 @@
 /* Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod */
 #include<iostream>
 using namespace std;
-void toh(int n,char sou,char aux,char des);          //function declaration
+void toh(int n,char sou,char aux,char des);          /*function declaration in which the arguments are n-no of disks ,sou-source rod ,aux-auxiliary rod,des-destination rod */
 int main(){
 	int n;
 	cout<<"enter the number of disks "<<endl;
@@ -10,14 +10,16 @@ int main(){
 	return 0;
 }
 void toh(int n,char sou,char aux,char des){                              //function definition
-	if (n==1)                                                        // base condition
-	{
+	if (n==1)             //base condition                                         
+	{     /* if there is only one disk then it directly moved from source to destination rod */
 		cout<<"move disk "<<n<<" from "<<sou<<" to "<<des<<" "<<endl;
 	}
 	else
 	{
-		toh(n-1,sou,des,aux);                                    // implement the function for n-1 disks
-		cout<<"move disk "<<n<<" from "<<sou<<" to "<<des<<" "<<endl;
-		toh(n-1,aux,sou,des);                                    // it will call it self until it statsify the base case
+	        /* if the disks are n then it calls again same function with n-1 disks and this time we change the rods order because we need to change disk from source to auxiliary rod */
+		toh(n-1,sou,des,aux);                                   
+		cout<<"move disk "<<n<<" from "<<sou<<" to "<<des<<" "<<endl; // function call itself again and again until it satisify the base condition 
+		/* after disk is changed from source to auxiliary now again we call toh function with n-1 disk and now the disk is moved from auiliary to destination */
+		toh(n-1,aux,sou,des);                                    
 	}
 }
