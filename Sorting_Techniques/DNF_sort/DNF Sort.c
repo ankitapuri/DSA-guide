750535f0b9b21a25ee9ea6b4c2e01849cd06b4b4
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-03-16 09:20:12 +0530

    Create DNF Sort.c

---
+++ Sorting_Techniques/DNF_sort/DNF Sort.c
@@ -0,0 +1,61 @@
+// A program to sort an array with 0, 1 and 2 
+
+#include <stdio.h> 
+  
+// Swapping a and b
+void swap(int* a, int* b); 
+  
+// Sorting the input array 
+void sort012(int a[], int arr_size) 
+{ 
+    int lo = 0; 
+    int hi = arr_size - 1; 
+    int mid = 0; 
+  
+    while (mid <= hi) { 
+        switch (a[mid]) { 
+        case 0: 
+            swap(&a[lo++], &a[mid++]); 
+            break; 
+        case 1: 
+            mid++; 
+            break; 
+        case 2: 
+            swap(&a[mid], &a[hi--]); 
+            break; 
+        } 
+    } 
+} 
+  
+/* UTILITY FUNCTIONS */
+void swap(int* a, int* b) 
+{ 
+    int temp = *a; 
+    *a = *b; 
+    *b = temp; 
+} 
+  
+/* Utility function to print array arr[] */
+void printArray(int arr[], int arr_size) 
+{ 
+    int i; 
+    for (i = 0; i < arr_size; i++) 
+        printf("%d ", arr[i]); 
+    printf("n"); 
+} 
+  
+/* driver program to test */
+int main() 
+{ 
+    int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 }; 
+    int arr_size = sizeof(arr) / sizeof(arr[0]); 
+    int i; 
+  
+    sort012(arr, arr_size); 
+  
+    printf("array after segregation "); 
+    printArray(arr, arr_size); 
+  
+    getchar(); 
+    return 0; 
+} 
