da4b50366e99fa92574377e0733f5674eba680ce
Abhinav <agrawal.abhinav95@gmail.com>
2021-03-21 15:43:46 +0530

    Added comments wherever required

--- Arrays/python/compareStringByFrequencyOfSmallestCharacter.py
+++ Arrays/python/compareStringByFrequencyOfSmallestCharacter.py
@@ -13,10 +13,13 @@ Output: [1]
 
 from collections import Counter
 def numSmallerByFrequency(queries, words):
-        fre_queries = fre(queries)
+    # Calculate the frequency of smallest character for each word of query array
+        fre_queries = fre(queries) 
+        # Calculate the frequency of smallest character for each word of words array & sort it in reverse order.
         fre_words = sorted(fre(words))[::-1]
         
         res = []
+        # compare reach frequency in fre_queries with each element of fre_words & increase count accordingly
         for q in fre_queries:
             count = 0
             for w in fre_words:
@@ -26,20 +29,24 @@ def numSmallerByFrequency(queries, words):
                     count += 1
             res.append(count)
         return res                        
-            
+#  A function to find the frequency of smallest character.
 def fre(arrs):
+    # Sort the array
         sorted_arrs = [sorted(arr) for arr in arrs]
         fre = []
         for arr in sorted_arrs:
             fre.append(list(Counter(arr).items())[0][1]) 
         return fre
 
+# Main begins here
 input_queries = input('Enter elements of a queries separated by space: ')
 print("\n")
+# This would split the input string separated by spaces into string array
 queries_list = input_queries.split()
 
 input_words = input('Enter elements of a words separated by space: ')
 print("\n")
+# This would split the input string separated by spaces into string array
 words_list = input_words.split()
 
 # print(queries_list)
