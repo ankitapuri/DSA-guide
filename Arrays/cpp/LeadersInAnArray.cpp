a08fa092379021cfa9b64b8328d8ee3b8b688b41
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-07-08 23:21:37 +0530

    Created LeadersInAnArray.cpp

---
+++ Arrays/cpp/LeadersInAnArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+void leaders(int arr[], int n)
+{
+	int curr_ldr = arr[n - 1];
+
+	cout<<curr_ldr<<" ";
+
+	for(int i = n - 2; i >= 0; i--)
+	{
+		if(curr_ldr < arr[i])
+		{
+			curr_ldr = arr[i];
+
+			cout<<curr_ldr<<" ";
+		}
+	}
+}
+
+
+
+int main() {
+	
+      int arr[] = {7, 10, 4, 10, 6, 5, 2}, n = 7;
+
+      leaders(arr, n);
+    
+}
